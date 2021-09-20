d14b50695aa86fb958982d3721fa4a1e86e7b3d3
Penny <coldblueriver@gmail.com>
2021-09-20 17:30:46 +0800

    Build version 2.0

---
+++ YoHome2.0/YoHome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YoHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
