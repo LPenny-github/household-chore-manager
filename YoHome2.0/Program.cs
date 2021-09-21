81422279f901740cd17e9ea6f4f50e0a694e1404
Penny <coldblueriver@gmail.com>
2021-09-22 00:41:36 +0800

    Add command `new`

--- YoHome2.0/Program.cs
+++ YoHome2.0/Program.cs
@@ -4,9 +4,36 @@ namespace YoHome
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] arguments)
         {
-            Console.WriteLine("Hello World!");
+            string GetArgument(int index, string name)
+            {
+                if (arguments.Length < 1)
+                {
+                    throw new ArgumentNullException(nameof(name), $"Argument `{name}` is empty.");
+                }
+                return arguments[index];
+            }
+            string command = GetArgument(0, nameof(command)).ToLowerInvariant();
+            HouseholdChoreManager householdChoreManager = new HouseholdChoreManager();
+            switch (command)
+            {
+                case "new":
+                    {
+                        int frequency = -1;
+                        bool isNumber = Int32.TryParse(arguments[2], out frequency);
+                        if (!isNumber || frequency < 0)
+                        {
+                            Console.WriteLine($"{arguments[1]}'s frequency({frequency}) is unreasonable.");
+                            break;
+                        }
+                        householdChoreManager.CreateHouseholdChore(arguments[1], frequency);
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"System don't recognize this command: {command}");
+                    break;
+            }
         }
     }
 }
