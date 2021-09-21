01febb1be776fa7ceb5a9ba81ba60932fb5dfca6
Penny <coldblueriver@gmail.com>
2021-09-21 17:10:21 +0800

    Add fuction `CreateHouseholdChore`

--- YoHome2.0/HouseholdChoreManager.cs
+++ YoHome2.0/HouseholdChoreManager.cs
@@ -1,9 +1,16 @@
 using System.Collections.Generic;
+using System;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
 
 namespace YoHome
 {
     public class HouseholdChoreManager
     {
+        int householdChoreSerialNumber = 0;
+        int recordSerialNumber = 0;
         Dictionary<int, string> SearchSimilarHouseholdChore(string keyword)
         {
             Dictionary<int, string> similarHouseholdChore = new Dictionary<int, string>();
@@ -17,6 +24,34 @@ namespace YoHome
             Dictionary<int, string> SimilarHouseholdChore = new Dictionary<int, string>();
             SimilarHouseholdChore = SearchSimilarHouseholdChore(candidateName);
             return SimilarHouseholdChore.Count == searchPattern;
+        }
+
+        void CreateHouseholdChore(string name, int frequency)
+        {
+            if (!HasCandidateExisted(name, 0)) { } ;
+            HouseholdChoreInformation householdChoreData = new HouseholdChoreInformation
+            {
+                HouseholdChoreSerialNumber = householdChoreSerialNumber + 1,
+                HouseholdChoreName = name,
+                IdealFrequency = frequency,
+                LastImplementedDate = new DateTime()
+            };
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            };
+            string SerializedString = JsonSerializer.Serialize<HouseholdChoreInformation>(householdChoreData,options);
+
+            try
+            {
+            File.AppendAllText("HouseholdChoreInformation.json", SerializedString.ToString());
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
         }
     }
 }
