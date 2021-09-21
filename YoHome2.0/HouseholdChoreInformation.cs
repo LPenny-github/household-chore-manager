81d417ac9b6d8d2c9d2677b89549a2d51f643e9c
Penny <coldblueriver@gmail.com>
2021-09-21 11:22:05 +0800

    Add `HouseholdChoreInformation.cs`

---
+++ YoHome2.0/HouseholdChoreInformation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace YoHome
+{
+    class HouseholdChoreInformation
+    {
+        public int SerialNumber;
+        public string HouseholdName;
+        public int IdealFrequency;
+        public DateTime LastImplementedDate;
+    }
+}
