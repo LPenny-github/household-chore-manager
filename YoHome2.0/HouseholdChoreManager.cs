4e6430475e986792a13cfaec5f61ba85b4240d0d
Penny <coldblueriver@gmail.com>
2021-09-21 16:29:37 +0800

    Add fuction `HasCandidateExisted`

--- YoHome2.0/HouseholdChoreManager.cs
+++ YoHome2.0/HouseholdChoreManager.cs
@@ -7,8 +7,16 @@ namespace YoHome
         Dictionary<int, string> SearchSimilarHouseholdChore(string keyword)
         {
             Dictionary<int, string> similarHouseholdChore = new Dictionary<int, string>();
-            
+
             return similarHouseholdChore;
-        }      
+        }
+
+
+        bool HasCandidateExisted(string candidateName, int searchPattern)
+        {
+            Dictionary<int, string> SimilarHouseholdChore = new Dictionary<int, string>();
+            SimilarHouseholdChore = SearchSimilarHouseholdChore(candidateName);
+            return SimilarHouseholdChore.Count == searchPattern;
+        }
     }
 }
