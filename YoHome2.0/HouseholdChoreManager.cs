762d1143c75c02f0bef0be916fbec30eeba395b5
Penny <coldblueriver@gmail.com>
2021-09-21 16:28:26 +0800

    Add fuction `SearchSimilarHouseholdChore'

---
+++ YoHome2.0/HouseholdChoreManager.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace YoHome
+{
+    public class HouseholdChoreManager
+    {
+        Dictionary<int, string> SearchSimilarHouseholdChore(string keyword)
+        {
+            Dictionary<int, string> similarHouseholdChore = new Dictionary<int, string>();
+            
+            return similarHouseholdChore;
+        }      
+    }
+}
