10301e59f4c34d0a9a0d7cd1f992fdfd7e4068c5
Penny <coldblueriver@gmail.com>
2021-09-27 20:18:37 +0800

    Update fuction `SearchSimilarHouseholdChore`

--- YoHome2.0/HouseholdChoreManager.cs
+++ YoHome2.0/HouseholdChoreManager.cs
@@ -4,31 +4,57 @@ using System.Text.Json;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using System.IO;
+using System.Linq;
 
 namespace YoHome
 {
+
     public class HouseholdChoreManager
     {
         int householdChoreSerialNumber = 0;
         int recordSerialNumber = 0;
-        Dictionary<int, string> SearchSimilarHouseholdChore(string keyword)
+        const string fileName = "HouseholdChoreInformation.json";
+
+        // HouseholdChoreInformation householdChoreInformation = new HouseholdChoreInformation();
+        List<HouseholdChoreInformation> householdChoreInformation = new List<HouseholdChoreInformation>();
+
+        List<HouseholdChoreInformation> JsonToList()
+        { 
+            if (File.Exists(fileName))
+            {
+                var householdChoreInformationJson = File.ReadAllText(fileName);
+                householdChoreInformation = JsonSerializer.Deserialize<List<HouseholdChoreInformation>>(householdChoreInformationJson);             
+            }
+            return householdChoreInformation;
+        }
+
+        string[] SearchSimilarHouseholdChore(string keyword)
         {
-            Dictionary<int, string> similarHouseholdChore = new Dictionary<int, string>();
-            // 不精確搜尋
+            string[] similarHouseholdChore = new string[householdChoreSerialNumber];
+
+            if (householdChoreSerialNumber < 1) // 家事序號 < 1，代表沒有任何家事資料
+            {
+                return similarHouseholdChore;
+            }
+
+            // 不精確搜尋 / fuzzy search 
+            similarHouseholdChore = householdChoreInformation.Where(h => h.HouseholdChoreName.Contains(keyword)).Select(n => n.HouseholdChoreName).ToArray();
+
             return similarHouseholdChore;
         }
 
 
-        bool HasCandidateExisted(string candidateName, int searchPattern)
+        bool AreCandidatesMatchingSearchPattern(string candidateName, int searchPattern, params string[] similarHouseholdChore)
         {
-            Dictionary<int, string> SimilarHouseholdChore = SearchSimilarHouseholdChore(candidateName);
-            // 精確搜尋
-            return SimilarHouseholdChore.Count == searchPattern;
+
+            // 進階搜尋 / Advanced Search
+            
+            return similarHouseholdChore.Length == searchPattern;
         }
 
         public void CreateHouseholdChore(string name, int frequency)
         {
-            if (!HasCandidateExisted(name, 0)) { };
+            if (!AreCandidatesMatchingSearchPattern(name, 0, SearchSimilarHouseholdChore(name))) { /* 無法建立家事 */  };
             HouseholdChoreInformation householdChoreData = new HouseholdChoreInformation
             {
                 HouseholdChoreSerialNumber = householdChoreSerialNumber + 1,
@@ -42,11 +68,10 @@ namespace YoHome
                 Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
             };
             string SerializedString = JsonSerializer.Serialize<HouseholdChoreInformation>(householdChoreData, options);
-            string filePath = "HouseholdChoreInformation.json";
-            
+
             try
             {
-                File.AppendAllText(filePath, SerializedString.ToString());
+                File.AppendAllText(fileName, SerializedString.ToString());
             }
             catch (IOException)
             {
