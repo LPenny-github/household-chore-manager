5d909733b0f7d56476b7f2acc12a943d94f55987
Penny <coldblueriver@gmail.com>
2021-09-28 10:42:08 +0800

    Update fuction `AreCandidatesMatchingSearchPatt...

--- YoHome2.0/HouseholdChoreManager.cs
+++ YoHome2.0/HouseholdChoreManager.cs
@@ -28,9 +28,9 @@ namespace YoHome
             return householdChoreInformation;
         }
 
-        string[] SearchSimilarHouseholdChore(string keyword)
+        List<string> SearchSimilarHouseholdChore(string keyword)
         {
-            string[] similarHouseholdChore = new string[householdChoreSerialNumber];
+            List<string> similarHouseholdChore = new List<string>();
 
             if (householdChoreSerialNumber < 1) // 家事序號 < 1，代表沒有任何家事資料
             {
@@ -38,18 +38,18 @@ namespace YoHome
             }
 
             // 不精確搜尋 / fuzzy search 
-            similarHouseholdChore = householdChoreInformation.Where(h => h.HouseholdChoreName.Contains(keyword)).Select(n => n.HouseholdChoreName).ToArray();
+            // similarHouseholdChore = householdChoreInformation.Where(h => h.HouseholdChoreName.Contains(keyword) || h.HouseholdChoreName.Any(h.HouseholdChoreName.Contains(keyword))).Select(n => n.HouseholdChoreName);
 
             return similarHouseholdChore;
         }
 
 
-        bool AreCandidatesMatchingSearchPattern(string candidateName, int searchPattern, params string[] similarHouseholdChore)
+        bool AreCandidatesMatchingSearchPattern(string candidateName, int searchPattern, List<string> similarHouseholdChore)
         {
 
             // 進階搜尋 / Advanced Search
-            
-            return similarHouseholdChore.Length == searchPattern;
+            string[] accurateHouseholdChore = similarHouseholdChore.Where(h => h.Contains(candidateName)).Select(n => n).ToArray();
+            return accurateHouseholdChore.Length == searchPattern;
         }
 
         public void CreateHouseholdChore(string name, int frequency)
