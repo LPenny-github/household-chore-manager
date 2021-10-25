2c7bb83d3862a6f17aab26ace4787ad07b9976c8
Penny <coldblueriver@gmail.com>
2021-10-25 17:11:25 +0800

    Rename class in `Tests`

---
+++ YoHome3/Tests/TestManagerMethod.cs
@@ -0,0 +1,15 @@
+using ConsoleApp;
+using Xunit;
+
+namespace Tests
+{
+    public class TestManagerMethod
+    {
+        HouseholdChoreManager householdChoreManager = new();
+        [Fact]
+        public void CreateNewHouseholdChore()
+        {
+            
+        }
+    }
+}
