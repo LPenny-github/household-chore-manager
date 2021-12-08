55ccfe7c71cbdd4fcb10ae92a607ce5f52cecdb4
Penny <coldblueriver@gmail.com>
2021-12-08 20:12:26 +0800

    Clean code

--- YoHome4/ConsoleApp/Program.cs
+++ YoHome4/ConsoleApp/Program.cs
@@ -34,51 +34,22 @@ namespace ConsoleApp
                     {
                         commandPurpose = "建立新家事";
 
-                        // user input: "new"
-                        if (arguments.Length == 1)
+                        var checkCommandNewResult = new CheckCommandNew().ReturnCommandNewResult(arguments);
+                        if (checkCommandNewResult.IsValid)
                         {
-                            errorMessage = "未輸入家事名稱與頻率";
-                        }
-
-                        // user input: "new 洗衣服"
-                        else if (arguments.Length == 2)
-                        {
-                            errorMessage = "未輸入家事頻率";
-                        }
-
-                        else if (arguments.Length == 3)
-                        {
-                            string name = arguments[1];
-                            int frequency = -1;
-                            bool isNumber = Int32.TryParse(arguments[2], out frequency);
-
-                            // user input: "new 洗衣服 !" or "new 洗衣服 0"
-                            if (!isNumber || frequency < 1)
+                            var result = new NewHouseholdChore().CreateHouseholdChore(arguments[1], checkCommandNewResult.frequency);
+                            if (result.valid)
                             {
-                                string userInputFrequency = arguments[2];
-                                errorMessage = $"家事頻率({userInputFrequency})不合理";
+                                message = new DataWriter().BuildNewChoreItem(commandPurpose, result.jsonString);
                             }
-
-                            // user input: "new 洗衣服 14"
                             else
                             {
-                                var result = newHouseholdChore.CreateHouseholdChore(arguments[1], frequency);
-                                if (result.valid)
-                                {
-                                    message = new DataWriter().BuildNewChoreItem(commandPurpose, result.jsonString);
-                                }
-                                else
-                                {
-                                    errorMessage = result.errorMessage;
-                                }
-
+                                errorMessage = result.errorMessage;
                             }
                         }
-
-                        // user input: "new 刷牙 10 好棒棒"
-                        else if (arguments.Length > 3)
+                        else
                         {
-                            errorMessage = "輸入過多參數或詞彙、數組間有多餘空白";
+                            errorMessage = checkCommandNewResult.errorMessage;
                         }
                     }
                     break;
