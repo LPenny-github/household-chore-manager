0bfd7bba2c5705b1b074a2c0442e0f8832d9951b
Penny <coldblueriver@gmail.com>
2021-12-08 16:56:24 +0800

    Clean code

--- YoHome4/ConsoleApp/Program.cs
+++ YoHome4/ConsoleApp/Program.cs
@@ -16,33 +16,34 @@ namespace ConsoleApp
                 return arguments[index];
             }
             string command = GetArgument(0, nameof(command)).ToLowerInvariant();
-            OperationResultStringMaker operationResultStringMaker = new();
+
+            string commandPurpose = "執行指令";
+            bool IsSuccessful = false;
+            string errorMessage = null;
             string message;
+            
             NewHouseholdChore newHouseholdChore = new();
             switch (command)
             {
                 case "": // user input: ""
                     {
-                        message = operationResultStringMaker.StringMaker("執行指令", false, "未輸入任何指令");
-                        Console.WriteLine(message);
+                        errorMessage = "未輸入任何指令";
                     }
                     break;
                 case "new":
                     {
-                        const string commandPurpose = "建立新家事";
+                        commandPurpose = "建立新家事";
 
                         // user input: "new"
                         if (arguments.Length == 1)
                         {
-                            message = operationResultStringMaker.StringMaker(commandPurpose, false, "未輸入家事名稱與頻率");
-                            Console.WriteLine(message);
+                            errorMessage = "未輸入家事名稱與頻率";
                         }
 
                         // user input: "new 洗衣服"
                         else if (arguments.Length == 2)
                         {
-                            message = operationResultStringMaker.StringMaker(commandPurpose, false, "未輸入家事頻率");
-                            Console.WriteLine(message);
+                            errorMessage = "未輸入家事頻率";
                         }
 
                         else if (arguments.Length == 3)
@@ -55,8 +56,7 @@ namespace ConsoleApp
                             if (!isNumber || frequency < 1)
                             {
                                 string userInputFrequency = arguments[2];
-                                message = operationResultStringMaker.StringMaker(commandPurpose, false, $"家事頻率({userInputFrequency})不合理");
-                                Console.WriteLine(message);
+                                errorMessage = $"家事頻率({userInputFrequency})不合理";
                             }
 
                             // user input: "new 洗衣服 14"
@@ -66,12 +66,10 @@ namespace ConsoleApp
                                 if (result.valid)
                                 {
                                     message = new DataWriter().BuildNewChoreItem(commandPurpose, result.jsonString);
-                                    Console.WriteLine(message);
                                 }
                                 else
                                 {
-                                    message = operationResultStringMaker.StringMaker(commandPurpose, result.valid, result.errorMessage);
-                                    Console.WriteLine(message);
+                                    errorMessage = result.errorMessage;
                                 }
 
                             }
@@ -80,16 +78,18 @@ namespace ConsoleApp
                         // user input: "new 刷牙 10 好棒棒"
                         else if (arguments.Length > 3)
                         {
-                            message = operationResultStringMaker.StringMaker(commandPurpose, false, "輸入過多參數或詞彙、數組間有多餘空白");
-                            Console.WriteLine(message);
+                            errorMessage = "輸入過多參數或詞彙、數組間有多餘空白";
                         }
                     }
                     break;
                 default:
-                    message = operationResultStringMaker.StringMaker("執行指令", false, $"找不到該指令({command})相應的動作");
-                    Console.WriteLine(message);
+                    errorMessage = $"找不到該指令({command})相應的動作";
                     break;
             }
+
+            OperationResultStringMaker operationResultStringMaker = new();
+            message = operationResultStringMaker.StringMaker(commandPurpose, IsSuccessful, errorMessage);
+            Console.WriteLine(message);
         }
     }
 }
